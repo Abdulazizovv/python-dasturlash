9394fde88abdbd47fe466b1b800144818fe337e4
abdulazizovv <aqllibola567@gmail.com>
2025-11-07 11:06:12 +0500

    yangi topshiriq yechdim

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 
 # ismni harflarini listga chiqarish
-ism = input("Ismingizni kiriting:>")
+# ism = input("Ismingizni kiriting:>")
 
-letters = []
+# letters = []
 
-for i in ism:
-    letters.append(i)
+# for i in ism:
+#     letters.append(i)
 
-print("Natija: ", letters)
+# print("Natija: ", letters)
+
+# listdagi takrorlangan elementlarni o'chirish
+
+lst = [1, 2, 2, 3, 4, 4, 5, 5]
+
+new_list = []
+
+for i in lst:
+    if i in new_list == False:
+        new_list.append(i)
+
+print("Natija: ", new_list)
